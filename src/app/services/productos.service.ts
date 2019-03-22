bf08e1533d4039202be3f09b1fbbc7e5d1ddab64
Estefa <estefania.carevich@gmail.com>
2019-03-22 01:54:06 -0300

    Pre prod

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../interface/producto-info.interface';
-import { Promise, reject } from 'q';
-import { resolve } from 'dns';
 
 @Injectable({
   providedIn: 'root'
